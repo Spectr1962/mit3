7bef68014dff5eac34e1bd056371dc54a8af3d4c
Spectr1962 <39295584+Spectr1962@users.noreply.github.com>
2026-08-25 19:36:11 +0300

    fix: добавлена команда seed в prisma/prisma.config.ts под v7

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -2,7 +2,12 @@ import 'dotenv/config'
 import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
+    schema: 'prisma/schema.prisma',
     datasource: {
         url: process.env.DATABASE_URL,
+    },
+    // ДОБАВЛЯЕМ СЕКЦИЮ ДЛЯ PRISMA V7:
+    migrations: {
+        seed: 'tsx prisma/seed.ts',
     },
 })
