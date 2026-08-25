2d4b0557a48434b883c2d6d669912a3331da36bc
Spectr1962 <39295584+Spectr1962@users.noreply.github.com>
2026-08-25 17:47:17 +0300

    fix: добавлен алиас post для роутера marketing в корневом файле root.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,10 +2,13 @@ import { marketingRouter } from "~/server/api/routers/marketing";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
- * Главный роутер сервера. Сюда подключен только наш маркетинг.
+ * Главный роутер сервера. Сюда подключен маркетинг.
  */
 export const appRouter = createTRPCRouter({
   marketing: marketingRouter,
+
+  // Добавляем алиас 'post', чтобы админка получила доступ к процедурам без изменения фронтенда!
+  post: marketingRouter,
 });
 
 export type AppRouter = typeof appRouter;
