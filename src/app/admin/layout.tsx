603f11573425f8540a34bdce7134630839782e8d
Spectr1962 <39295584+Spectr1962@users.noreply.github.com>
2026-08-28 15:16:59 +0300

    fix admin credentials session

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,12 +1,11 @@
 import { redirect } from "next/navigation";
 import { auth } from "~/server/auth";
 
-const adminEmail = "larionov.igor1987@yandex.ru";
-
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
     const session = await auth();
+    const adminLogin = process.env.AUTH_ADMIN_LOGIN;
 
-    if (session?.user.email !== adminEmail) {
+    if (!session?.user || !adminLogin || session.user.id !== adminLogin) {
         redirect("/login?callbackUrl=/admin");
     }
 
