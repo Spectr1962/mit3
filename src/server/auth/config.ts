15cdfeb6aa9716b46fc8f79443447d12f1768d61
Spectr1962 <39295584+Spectr1962@users.noreply.github.com>
2026-08-28 12:44:59 +0300

    fix github oauth issuer

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -23,7 +23,6 @@ export const authConfig = {
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      issuer: "https://github.com/login/oauth",
     })
   ],
   secret: process.env.AUTH_SECRET, // Обязательный секрет для шифрования куки локально
